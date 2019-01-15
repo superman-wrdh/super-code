73596f9870ed472f12a155e69f3c6ed8ef35b559
superman-wrdh <13419655360@163.com>
2019-01-15 19:24:00 +0800

    interesting code

---
+++ super-algorithm/src/main/java/cn/hc/algorithm/test/TryDemo.java
@@ -0,0 +1,26 @@
+package cn.hc.algorithm.test;
+
+public class TryDemo {
+    public static void main(String[] args) {
+        int j = query();
+        System.out.println(j);
+    }
+
+    public static int query() {
+        int i = 0;
+        try {
+            System.out.print("try\n");
+            return i += 10;
+        } catch (Exception e) {
+            System.out.print("catch\n");
+            i += 20;
+        } finally {
+            System.out.print("finally-i:" + i + "\n");
+            i += 10;
+            System.out.print("finally\n");
+            return i;
+        }
+    }
+}
+
+
