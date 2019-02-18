1f6692994b17c9b3965076908e276ebca57ce370
superman-wrdh <13419655360@163.com>
2019-02-19 00:00:52 +0800

    sort

---
+++ super-algorithm/src/main/java/cn/hc/algorithm/sort/AllSort.java
@@ -0,0 +1,221 @@
+package cn.hc.algorithm.sort;
+
+import java.util.Arrays;
+
+/**
+ * Created by super on 2019/2/18
+ https://mp.weixin.qq.com/s?__biz=MzU0OTk3ODQ3Ng==&mid=2247484600&idx=1&sn=a6e6b4633a3d643a130764bfa7dce6dc&chksm=fba6ecbbccd165ad16bb91181b5e141806321277f87dc84294f1d0dc1afaa9d33b75998424c6&scene=0&subscene=131&clicktime=1550290110&ascene=7&devicetype=android-28&version=27000336&nettype=WIFI&abtest_cookie=BAABAAoACwATABQABAAklx4AWpkeAJuZHgCdmR4AAAA%3D&lang=zh_CN&pass_ticket=fABMn7wzpF6F8h3HUs6vnbOtGXDVTIewBU%2ByNRxyv2zo5TJNHT8RU5IBCOFvG6hc&wx_header=1
+ */
+public class AllSort {
+    public static void main(String[] args) {
+
+    }
+
+    //插入排序
+
+    /**
+     * 插入排序（Insertion-Sort）的算法描述是一种简单直观的排序算法。
+     * 它的工作原理是通过构建有序序列，对于未排序数据，在已排序序列中从后向前扫描，找到相应位置并插入。
+     * 插入排序在实现上，通常采用in-place排序（即只需用到O(1)的额外空间的排序），因而在从后向前扫描过程中，需要反复把已排序元素逐步向后挪位，为最新元素提供插入空间。
+     * 3.1 算法描述
+     * 一般来说，插入排序都采用in-place在数组上实现。具体算法描述如下：
+     * 从第一个元素开始，该元素可以认为已经被排序；
+     * 取出下一个元素，在已经排序的元素序列中从后向前扫描；
+     * 如果该元素（已排序）大于新元素，将该元素移到下一位置；
+     * 重复步骤3，直到找到已排序的元素小于或者等于新元素的位置；
+     * 将新元素插入到该位置后；
+     * 重复步骤2~5。
+     */
+    public static int[] insertionSort(int[] array) {
+        if (array.length == 0)
+            return array;
+        int current;
+        for (int i = 0; i < array.length - 1; i++) {
+            current = array[i + 1];
+            int preIndex = i;
+            while (preIndex >= 0 && current < array[preIndex]) {
+                array[preIndex + 1] = array[preIndex];
+                preIndex--;
+            }
+            array[preIndex + 1] = current;
+        }
+        return array;
+    }
+
+    //选择排序
+
+    /**
+     * 表现最稳定的排序算法之一，因为无论什么数据进去都是O(n2)的时间复杂度，所以用到它的时候，数据规模越小越好。
+     * <p>
+     * 唯一的好处可能就是不占用额外的内存空间了吧。理论上讲，选择排序可能也是平时排序一般人想到的最多的排序方法了吧。
+     * <p>
+     * 选择排序(Selection-sort)是一种简单直观的排序算法。
+     * <p>
+     * 它的工作原理：首先在未排序序列中找到最小（大）元素，存放到排序序列的起始位置。
+     * <p>
+     * 然后，再从剩余未排序元素中继续寻找最小（大）元素，然后放到已排序序列的末尾。以此类推，直到所有元素均排序完毕。
+     * <p>
+     * 2.1 算法描述
+     * n个记录的直接选择排序可经过n-1趟直接选择排序得到有序结果。
+     * <p>
+     * 具体算法描述如下：
+     * 初始状态：无序区为R[1..n]，有序区为空；
+     * 第i趟排序(i=1,2,3…n-1)开始时，当前有序区和无序区分别为R[1..i-1]和R(i..n）。
+     * 该趟排序从当前无序区中-选出关键字最小的记录 R[k]，将它与无序区的第1个记录R交换，使R[1..i]和R[i+1..n)分别变为记录个数增加1个的新有序区和记录个数减少1个的新无序区；
+     * n-1趟结束，数组有序化了。
+     * <p>
+     * 最佳情况：T(n) = O(n2)
+     * 最差情况：T(n) = O(n2)
+     * 平均情况：T(n) = O(n2)
+     */
+    public static int[] selectionSort(int[] array) {
+        if (array.length == 0)
+            return array;
+        for (int i = 0; i < array.length; i++) {
+            int minIndex = i;
+            for (int j = i; j < array.length; j++) {
+                if (array[j] < array[minIndex]) //找到最小的数
+                    minIndex = j; //将最小数的索引保存
+            }
+            int temp = array[minIndex];
+            array[minIndex] = array[i];
+            array[i] = temp;
+        }
+        return array;
+    }
+
+    //快速排序
+    /**
+     * 快速排序的基本思想：通过一趟排序将待排记录分隔成独立的两部分，其中一部分记录的关键字均比另一部分的关键字小，则可分别对这两部分记录继续进行排序，以达到整个序列有序。
+     * 快速排序使用分治法来把一个串（list）分为两个子串（sub-lists）。具体算法描述如下：
+     * 从数列中挑出一个元素，称为 “基准”（pivot）；
+     * 重新排序数列，所有元素比基准值小的摆放在基准前面，所有元素比基准值大的摆在基准的后面（相同的数可以到任一边）。在这个分区退出之后，该基准就处于数列的中间位置。这个称为分区（partition）操作；
+     * 递归地（recursive）把小于基准值元素的子数列和大于基准值元素的子数列排序。
+     */
+    /**
+     * 快速排序方法
+     *
+     * @param array
+     * @param start
+     * @param end
+     * @return
+     */
+    public static int[] QuickSort(int[] array, int start, int end) {
+        if (array.length < 1 || start < 0 || end >= array.length || start > end) return null;
+        int smallIndex = partition(array, start, end);
+        if (smallIndex > start)
+            QuickSort(array, start, smallIndex - 1);
+        if (smallIndex < end)
+            QuickSort(array, smallIndex + 1, end);
+        return array;
+    }
+
+    /**
+     * 快速排序算法——partition
+     *
+     * @param array
+     * @param start
+     * @param end
+     * @return
+     */
+    public static int partition(int[] array, int start, int end) {
+        int pivot = (int) (start + Math.random() * (end - start + 1));
+        int smallIndex = start - 1;
+        swap(array, pivot, end);
+        for (int i = start; i <= end; i++)
+            if (array[i] <= array[end]) {
+                smallIndex++;
+                if (i > smallIndex)
+                    swap(array, i, smallIndex);
+            }
+        return smallIndex;
+    }
+
+    /**
+     * 交换数组内两个元素
+     *
+     * @param array
+     * @param i
+     * @param j
+     */
+    public static void swap(int[] array, int i, int j) {
+        int temp = array[i];
+        array[i] = array[j];
+        array[j] = temp;
+    }
+    //******************************************************
+
+    //希尔排序
+
+    /**
+     * 希尔排序是希尔（Donald Shell）于1959年提出的一种排序算法。
+     * 希尔排序也是一种插入排序，它是简单插入排序经过改进之后的一个更高效的版本，也称为缩小增量排序，同时该算法是冲破O(n2）的第一批算法之一。
+     * 它与插入排序的不同之处在于，它会优先比较距离较远的元素。希尔排序又叫缩小增量排序。
+     * 希尔排序是把记录按下表的一定增量分组，对每组使用直接插入排序算法排序；
+     * 随着增量逐渐减少，每组包含的关键词越来越多，当增量减至1时，整个文件恰被分成一组，算法便终止。
+     * 最佳情况：T(n) = O(nlog2 n)
+     * 最坏情况：T(n) = O(nlog2 n)
+     * 平均情况：T(n) =O(nlog2n)
+     */
+    public static int[] ShellSort(int[] array) {
+        int len = array.length;
+        int temp, gap = len / 2;
+        while (gap > 0) {
+            for (int i = gap; i < len; i++) {
+                temp = array[i];
+                int preIndex = i - gap;
+                while (preIndex >= 0 && array[preIndex] > temp) {
+                    array[preIndex + gap] = array[preIndex];
+                    preIndex -= gap;
+                }
+                array[preIndex + gap] = temp;
+            }
+            gap /= 2;
+        }
+        return array;
+    }
+
+    //归并排序
+
+    /**
+     * 和选择排序一样，归并排序的性能不受输入数据的影响，但表现比选择排序好的多，因为始终都是O(n log n）的时间复杂度。
+     * 代价是需要额外的内存空间。
+     * 归并排序是建立在归并操作上的一种有效的排序算法。该算法是采用分治法（Divide and Conquer）的一个非常典型的应用。
+     * 归并排序是一种稳定的排序方法。将已有序的子序列合并，得到完全有序的序列；即先使每个子序列有序，再使子序列段间有序。若将两个有序表合并成一个有序表，称为2-路归并。
+     * 5.1 算法描述
+     * 把长度为n的输入序列分成两个长度为n/2的子序列；
+     * 对这两个子序列分别采用归并排序；
+     * 将两个排序好的子序列合并成一个最终的排序序列。
+     */
+    public static int[] MergeSort(int[] array) {
+        if (array.length < 2) return array;
+        int mid = array.length / 2;
+        int[] left = Arrays.copyOfRange(array, 0, mid);
+        int[] right = Arrays.copyOfRange(array, mid, array.length);
+        return merge(MergeSort(left), MergeSort(right));
+    }
+
+    /**
+     * 归并排序——将两段排序好的数组结合成一个排序数组
+     *
+     * @param left
+     * @param right
+     * @return
+     */
+    public static int[] merge(int[] left, int[] right) {
+        int[] result = new int[left.length + right.length];
+        for (int index = 0, i = 0, j = 0; index < result.length; index++) {
+            if (i >= left.length)
+                result[index] = right[j++];
+            else if (j >= right.length)
+                result[index] = left[i++];
+            else if (left[i] > right[j])
+                result[index] = right[j++];
+            else
+                result[index] = left[i++];
+        }
+        return result;
+    }
+
+
+}
