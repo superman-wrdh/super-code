712148d539dbe0b63c9bfc88cf41e9860b33771f
superman-wrdh <13419655360@163.com>
2019-08-26 22:53:56 +0800

    update

---
+++ super-http-util/src/main/java/cn/hc/http/Tomdog.java
@@ -0,0 +1,46 @@
+package cn.hc.http;
+
+import java.io.IOException;
+import java.io.OutputStream;
+import java.net.ServerSocket;
+import java.net.Socket;
+
+public class Tomdog {
+    private ServerSocket server;
+
+    public static void main(String[] args) throws IOException {
+        Tomdog tomdog = new Tomdog(8080);
+        tomdog.start();
+    }
+
+    public Tomdog(int port) throws IOException {
+        try {
+            server = new ServerSocket(port);
+            System.out.println("tomdog start:" + port);
+        } catch (IOException e) {
+            server.close();
+        }
+    }
+
+    public void start() throws IOException {
+        this.receive(server);
+    }
+
+    public void receive(ServerSocket server) throws IOException {
+        while (true) {
+            Socket socket = server.accept();
+            OutputStream writer = socket.getOutputStream();
+            String hello = "GET HTTP/1.1\r\n" +
+                    "Content-Type: text/html;charset=utf-8\r\n" +
+                    "\n\n" +
+                    "<h5>hello tomdog</h5>";
+
+            writer.write(hello.getBytes());
+            writer.flush();
+            socket.close();
+            System.out.println("over");
+        }
+
+    }
+}
+
