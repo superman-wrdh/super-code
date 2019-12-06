de6757a84b9be9e8de951fbfb80d0359b3568f9e
superman-wrdh <13419655360@163.com>
2019-12-06 11:13:32 +0800

    update

--- turing-chat-robot/src/main/java/cn/hc/robot/service/TuringChatService.java
+++ turing-chat-robot/src/main/java/cn/hc/robot/service/TuringChatService.java
@@ -2,9 +2,11 @@ package cn.hc.robot.service;
 
 import cn.hc.http.Request;
 import cn.hc.http.Response;
+import com.google.gson.Gson;
 import net.sf.json.JSONObject;
 import org.apache.http.entity.ContentType;
 import org.springframework.stereotype.Service;
+
 import java.nio.charset.Charset;
 
 /**
@@ -12,30 +14,64 @@ import java.nio.charset.Charset;
  */
 @Service
 public class TuringChatService {
-    private static final String url="http://www.tuling123.com/openapi/api"; //图灵聊天请求地址
-    private static final String key="d1e264707c1b44c8b07584a16549c2de"; //key 妥善保管
-    private JSONObject paramaters=null;
-    public Response send(String info)throws Exception{
+    private static final String url = "http://www.tuling123.com/openapi/api"; //图灵聊天请求地址
+    private static final String key = "d1e264707c1b44c8b07584a16549c2de"; //key 妥善保管
+    private JSONObject paramaters = null;
+
+    public Response send(String info) throws Exception {
         System.out.println(info);
-        paramaters=new JSONObject();
-        paramaters.put("key",key);
-        paramaters.put("info",info);
-        Request r=new Request(url);
+        paramaters = new JSONObject();
+        paramaters.put("key", key);
+        paramaters.put("info", info);
+        Request r = new Request(url);
         r.contentType(ContentType.APPLICATION_JSON);
-        r.body(paramaters.toString(),"UTF-8");
+        r.body(paramaters.toString(), "UTF-8");
         return r.execute();
     }
 
-    public JSONObject getJSONMsg(Response response){
+    public JSONObject getJSONMsg(Response response) {
         response.setCharset(Charset.forName("utf-8"));
         String string = response.string();
-        JSONObject jsonObject= JSONObject.fromObject(string);
+        JSONObject jsonObject = JSONObject.fromObject(string);
         return jsonObject;
     }
 
-    public static void main(String[] args) throws Exception {
-        TuringChatService turingChatService = new TuringChatService();
-        Response response = turingChatService.send("hello");
-        System.out.println(turingChatService.getJSONMsg(response));
+    public Result getResult(Response response) {
+        response.setCharset(Charset.forName("utf-8"));
+        String string = response.string();
+        Gson gson = new Gson();
+        Result result = gson.fromJson(string, Result.class);
+        return result;
+    }
+
+//    public static void main(String[] args) throws Exception {
+//        TuringChatService turingChatService = new TuringChatService();
+//        Response response = turingChatService.send("hello");
+//        System.out.println(turingChatService.getJSONMsg(response));
+//        Result result = turingChatService.getResult(response);
+//        System.out.println(turingChatService.getJSONMsg(response));
+//        System.out.println(result);
+//    }
+}
+
+
+class Result {
+    private int code;
+    private String text;
+
+    public void setCode(int code) {
+        this.code = code;
+    }
+
+    public void setText(String text) {
+        this.text = text;
+    }
+
+    public int getCode() {
+        return code;
+    }
+
+    public String getText() {
+        return text;
     }
 }
